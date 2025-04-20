7cdee36ca96eda1b1e9cc930c8bbd68e537dd5af
rbalendra <rj.balendra@gmail.com>
2025-04-20 12:24:46 +1000

    fixes card css to view thumbnail properly

--- src/components/bookcard/BookCard.jsx
+++ src/components/bookcard/BookCard.jsx
@@ -7,24 +7,36 @@ const BookCard = ({ book }) => {
 	const authors = volumeInfo.authors?.join(', ') || 'Unknown Author'; //if authors is undefined, set to 'Unknown Author'
 	const thumbnail = volumeInfo.imageLinks?.thumbnail || 'No Image Available'; //optional chaining to avoid errors if imageLinks is undefined
 	const description =
-		searchInfo?.textSnippet ||
-		volumeInfo.description?.slice(1, 100) + '...' ||
-		'No Description Available'; //if description is undefined, set to 'No Description Available'
+		volumeInfo.description?.slice(0, 400) + '...' || 'No Description Available'; //if description is undefined, set to 'No Description Available'
 	const categories =
 		volumeInfo.categories?.join(', ') || 'No Categories Available'; //if categories is undefined, set to 'No Categories Available'
 
 	return (
 		<div className={styles.card}>
-			{thumbnail !== 'No Image Available' ? (
-				<img src={thumbnail} alt={title} className={styles.thumbnail} />
+			{thumbnail ? (
+				<div className={styles.thumbnailContainer}>
+					<img
+						src={thumbnail}
+						alt={`${title} cover`}
+						className={styles.thumbnail}
+					/>
+				</div>
 			) : (
-				<div className={styles.placeholder}>No Image</div>
+				<div className={styles.placeholder}>No image</div>
 			)}
 			<div className={styles.content}>
 				<h2 className={styles.title}>{title}</h2>
 				<p className={styles.authors}>{authors}</p>
 				<p className={styles.description}>{description}</p>
 				<p className={styles.categories}>{categories}</p>
+			</div>
+
+			<div className={styles.footer}>
+				<button
+					className={styles.detailsButton}
+					onClick={() => console.log('Details clicked')}>
+					Details
+				</button>
 			</div>
 		</div>
 	);
