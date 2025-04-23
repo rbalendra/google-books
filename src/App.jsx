1c979f7925b3d40ccac4fe96aca6ac1888a96b77
rbalendra <rj.balendra@gmail.com>
2025-04-23 11:05:06 +1000

    adds loading animation

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,45 @@ import Layout from './components/container/Layout';
 import BookList from './components/booklist/BookList';
 import { fetchBooks } from './utils/booksAPI';
 import { useState, useEffect } from 'react';
+import { FourSquare } from 'react-loading-indicators'; //loading animation
+
+/* this  is the main component of the app
+   it will hold the state of the app and pass it down to the children components such as SearchBar
+   it will also handle the api calls and pass the results down to the children components such as BookList
+*/
 
 function App() {
 	const [books, setBooks] = useState([]); //hold the search results
+	const [loading, setLoading] = useState(true); //created for loading states
 
 	const handleSearch = async (query) => {
-		//trigger the api call (possibly can use useEffect here)
-		const results = await fetchBooks(query);
-		setBooks(results);
+		try {
+			setLoading(true); // Set loading to true when starting a search
+			const results = await fetchBooks(query);
+			setBooks(results);
+			setLoading(false);
+		} catch (error) {
+			console.error('Error fetching books:', error);
+			setLoading(false);
+		}
 	};
 
 	useEffect(() => {
 		handleSearch('non-fiction');
 	}, []);
+
+	if (loading) {
+		return (
+			<div className='loading'>
+				<FourSquare
+					color={['#3a4a3d', '#a7c957', '#bc4749', '#1d384a']}
+					size='large'
+					text='loading books...'
+					textColor=''
+				/>
+			</div>
+		); //loading state
+	}
 
 	return (
 		<Layout>
--- src/utils/booksAPI.js
+++ src/utils/booksAPI.js
@@ -2,6 +2,9 @@ const BASE_URL = 'https://www.googleapis.com/books/v1/volumes';
 const API_KEY = import.meta.env.VITE_GOOGLE_BOOKS_API_KEY;
 //https://vite.dev/guide/env-and-mode
 
+/* this function fetches the books from google books API based on the search query. It makes an async request to the API and returns the results.
+It handles errors gracefully by logging them to the console and returning an empty array in case of an error. */
+
 export async function fetchBooks(query) {
 	try {
 		//https://developers.google.com/books/docs/v1/using
